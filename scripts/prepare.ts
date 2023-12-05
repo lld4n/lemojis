2521ffce5ec69edb16befbbfb0fd5ca07dfe2db7
lldan <safinildan@gmail.com>
2023-12-05 18:37:10 +0300

    добавлены скрипты

---
+++ scripts/prepare.ts
@@ -0,0 +1,78 @@
+import ky from "ky";
+import * as fs from 'fs';
+import type { Emoji, EmojiOriginal } from "./types.js";
+import { skins } from "./constants.js";
+
+
+async function createFirst() {
+  const json = ky.get("https://cdn.jsdelivr.net/npm/emoji-datasource/emoji.json");
+  const data: Array<EmojiOriginal> = await json.json();
+  let obj: {
+    [key: string]: Emoji;
+  } = {};
+
+  for (let i = 0; i < data.length; i++) {
+    let buffer: Emoji = {
+      i: data[i].image,
+      u: data[i].unified,
+      h: [
+        data[i].has_img_apple,
+        data[i].has_img_google,
+        data[i].has_img_facebook,
+        data[i].has_img_twitter,
+      ],
+      a: data[i].added_in,
+    };
+
+    if (data[i].skin_variations) {
+      buffer.s = [];
+      for (let key in data[i].skin_variations) {
+        let skin = skins[key];
+        buffer.s.push(i + "_" + skin);
+        obj[i + "_" + skin] = {
+          i: data[i].skin_variations[key].image,
+          u: data[i].skin_variations[key].unified,
+          h: [
+            data[i].skin_variations[key].has_img_apple,
+            data[i].skin_variations[key].has_img_google,
+            data[i].skin_variations[key].has_img_facebook,
+            data[i].skin_variations[key].has_img_twitter,
+          ],
+          a: data[i].skin_variations[key].added_in,
+        };
+      }
+    }
+    obj[i] = buffer;
+  }
+  let file = "export const emojis = " + JSON.stringify(obj) + ";";
+  console.log(Object.keys(obj).length);
+  fs.writeFileSync("../lib/data/emojis.ts", file);
+}
+
+async function createSecond() {
+  const json = ky.get("https://cdn.jsdelivr.net/npm/emoji-datasource/emoji.json");
+  const data: Array<EmojiOriginal> = await json.json();
+  let obj: {
+    [key: string]: {
+      [key: string]: string[];
+    };
+  } = {};
+  for (let i = 0; i < data.length; i++) {
+    if (obj[data[i].category]) {
+      if (obj[data[i].category][data[i].subcategory]) {
+        obj[data[i].category][data[i].subcategory].push(String(i));
+      } else {
+        obj[data[i].category][data[i].subcategory] = [String(i)];
+      }
+    } else {
+      obj[data[i].category] = {};
+      obj[data[i].category][data[i].subcategory] = [String(i)];
+    }
+  }
+  let file = "export const category = " + JSON.stringify(obj) + ";";
+  console.log(Object.keys(obj).length);
+  fs.writeFileSync("../lib/data/category.ts", file);
+}
+
+await createFirst();
+await createSecond();
---
+++ scripts/types.ts
@@ -0,0 +1,42 @@
+export type Emoji = {
+  // image
+  i: string;
+  // unified
+  u: string;
+  // has
+  h: [boolean, boolean, boolean, boolean];
+  // skins
+  s?: string[];
+  // added
+  a: string;
+};
+
+export type EmojiOriginal = {
+  name: string;
+  unified: string;
+  non_qualified: string;
+  docomo: string;
+  au: string;
+  softbank: string;
+  google: string;
+  image: string;
+  sheet_x: number;
+  sheet_y: number;
+  short_name: string;
+  short_names: string[];
+  text: string | null;
+  texts: string | null;
+  category: string;
+  subcategory: string;
+  sort_order: number;
+  added_in: string;
+  has_img_apple: boolean;
+  has_img_google: boolean;
+  has_img_twitter: boolean;
+  has_img_facebook: boolean;
+  skin_variations: {
+    [key: string]: EmojiOriginal;
+  };
+  obsoletes: string;
+  obsoleted_by: string;
+};
